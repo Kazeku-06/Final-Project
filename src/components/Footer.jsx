e4ac13d308882646d9d5a014bcc82d03468fd72e
Kazeku-06 <tssytari@gmail.com>
2025-10-07 14:44:26 +0700

    commit after host and adjusment again and again  in vercel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { APP_NAME } from '../utils/constants';
 
 const Footer = () => {
   return (
-    <footer className="bg-base-100 text-base-content py-8 mt-16">
+    <footer className="bg-base-200 text-base-content py-8 mt-16">
       <div className="container mx-auto px-4 text-center">
         <p>&copy; {new Date().getFullYear()} {APP_NAME}. All rights reserved.</p>
       </div>
